1a7da111f2eff5c57291900d1e2bb8704b9d608b
임성규 <tjdrb7021@gmail.com>
2024-08-27 14:34:03 +0900

    내용추가

--- test.java
+++ test.java
@@ -1,4 +1,5 @@
 public class test{
-  public static void main(String args[])[
-  System.out.println("Hi");
+  public static void main(String args[]){
+    System.out.println("Hi");
+    System.out.println("내용추가");
   ]
